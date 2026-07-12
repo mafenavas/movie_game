c7208ad85a05c64b3f2beb33b48a9d5a419cd22a
maria navas <mafis_2707@hotmail.com>
2026-07-11 20:59:12 -0400

    User interface 1

--- movie_game.py
+++ movie_game.py
@@ -5,69 +5,77 @@ movies = None
 
 with open ('movie_game.json') as f:
     movies = json.load(f)
-    print(movies)
 
-# 1. User Add a movie
+for movie in movies:
+    print(f"Movie: {movie}")
+    for key, value in movies[movie].items():
+        print(f"{key}: {value}")
+    print()
 
-movie = input('Please add a movie ')
-year = input('Add the year the movie was released ')
-genre = input('Type the genre of the movie ')
-director = input('Who is the director of the movie ')
-actors = input('List three actors from the movie ')
+print("Welcome to Movie Directory. Let's start")
 
-movies[movie] = {
+user_choice= (str(input('Please choose the action you want to do: ' \
+'\n1. Add a new movie ' \
+'\n2. Edit an existing movie ' \
+'\n3. Delete a movie ' \
+'\n4. Search a movie: ')))
+
+if user_choice == '1':
+    movie = input('Please add a movie ')
+    year = input('Add the year the movie was released ')
+    genre = input('Type the genre of the movie ')
+    director = input('Who is the director of the movie ')
+    actors = input('List three actors from the movie ')
+
+    movies[movie] = {
     'year': year,
     'genre': genre,
     'director': director,
     'actors': actors
-}
+    }
 
-with open('movie_game.json', 'w') as f:
-    json.dump(movies, f)
+    with open('movie_game.json', 'w') as f:
+        json.dump(movies, f)
 
-for movie in movies:
-    print(f"Movie: {movie}")
-    for key, value in movies[movie].items():
-        print(f"{key}: {value}")
-    print()
+    for movie in movies:
+        print(f"Movie: {movie}")
+        for key, value in movies[movie].items():
+         print(f"{key}: {value}")
+        print()
 
+elif user_choice == '2':
+    edit_movie = input('Please type the movie do you want to update: ')
+    edit_details = input('Please type what kind of info do you want to update: ')
+    user_new_info = input('Type the new info: ')
+    
+    movies[edit_movie][edit_details] = user_new_info
 
-# 2. User can edit any existing movie by updating its info
+    with open('movie_game.json', 'w') as f:
+        json.dump(movies, f)
 
-edit_movie = input('Please type the movie do you want to update: ')
-edit_details = input('Please type what kind of info do you want to update: ')
-user_new_info = input('Type the new info: ')
+elif user_choice == '3':
+    deleted_movie = input('Please type the movie you want to delete: ')
 
-movies[edit_movie][edit_details] = user_new_info
+    del movies[deleted_movie]
 
-with open('movie_game.json', 'w') as f:
-    json.dump(movies, f)
-
-# 3. User can delete any movie
-
-deleted_movie = input('Please type the movie you want to delete: ')
-
-del movies[deleted_movie]
-
-with open('movie_game.json', 'w') as f:
-    json.dump(movies, f)
+    with open('movie_game.json', 'w') as f:
+        json.dump(movies, f)
 
 
-for movie in movies:
-    print(f"Movie: {movie}")
-    for key, value in movies[movie].items():
-        print(f"{key}: {value}")
-    print()
+    for movie in movies:
+        print(f"Movie: {movie}")
+        for key, value in movies[movie].items():
+            print(f"{key}: {value}")
+        print()
 
-# 4. User can search any movie and its info
+else:
+    search_type = input('Please type the info related to the movie you want to search: ')
+    search_info = input('Please the info related to the movie: ')
 
-search_type = input('Please type the info related to the movie you want to search: ')
-search_info = input('Please the info related to the movie: ')
-
-for movie in movies:
-    for key, value in movies[movie].items():
-        if str(key) == search_type and str(value) == search_info:
-            print(f"{movie}")
+    for movie in movies:
+        for key, value in movies[movie].items():
+            if str(key) == search_type and str(value) == search_info:
+                print(f"{movie}")
 
 
 
